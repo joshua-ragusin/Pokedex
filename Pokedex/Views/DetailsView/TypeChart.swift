0ec57da2a2b4cb04e04306fb6b17e66b678a01f9
joshua-ragusin <ragusinj@gmail.com>
2023-06-04 17:39:45 -0500

    Added file for type chart for details view

---
+++ Pokedex/Views/DetailsView/TypeChart.swift
@@ -0,0 +1,16 @@
+//
+//  TypeChart.swift
+//  Pokedex
+//
+//  Created by Joshua Ragusin on 6/4/23.
+//
+
+import SwiftUI
+
+struct TypeChart: View {
+    private let pokemon: PokemonResult
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
